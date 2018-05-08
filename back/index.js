86c4853c44d247dc858e44306cb5f2e2bfdddde7
BayanMawed <bmaweed@gmail.com>
2018-05-08 18:18:15 +0300

    Srecond Part

--- back/index.js
+++ back/index.js
@@ -1,35 +1,27 @@
-const http = require('http')
-
-const server = http.createServer()
-
-const handleRequest = (req, res) => {
-  res.end('ok')
-}
-
-server.on('request', handleRequest)
-server.listen(8888, ()=> console.log(`server is ready`))
-const io = require('socket.io')(server);
-
-let globalNumber = 0
-
-io.on('connection', (socket) => {
-  
-  console.log('a user connected')
-	
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-	
-  socket.on('increment', () => {
-    globalNumber++
-    io.emit('number:change',globalNumber)
-  });
-
-  socket.on('decrement', () => {
-    globalNumber--
-    io.emit('number:change',globalNumber)
-  });
-
-  socket.emit('number:change',globalNumber)
-
-});
+const http = require('http') 
+const server = http.createServer() 
+const catNames = require('cat-names'); 
+const handleRequest = (req, res) => { 
+    res.end('ok!') } 
+server.on('request', handleRequest) 
+server.listen(8888, () => {
+    console.log('server is ready')}) 
+const io = require('socket.io')(server); 
+let globalNumber = 0 
+io.on('connection', (socket) => { 
+    const username = catNames.random() 
+    console.log('a user called ' + username + ' has connected') 
+    io.emit('user:new', username) 
+    socket.emit('user:me', username) 
+    socket.on('disconnect', () => { 
+    console.log('user disconnected'); 
+  }); 
+  socket.on('increment', () => { 
+      globalNumber++ 
+      io.emit('number:change', globalNumber) 
+    }); 
+  socket.on('decrement', () => { 
+      globalNumber-- 
+      io.emit('number:change', globalNumber) 
+    }); 
+  socket.emit('number:change', globalNumber) });
--- front/src/App.js
+++ front/src/App.js
@@ -1,44 +1,67 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import io from 'socket.io-client';
 import './App.css';
-
-
+import io from  'socket.io-client'
 
 class App extends Component {
 
-  state = { socket:null, globalNumber:0 }
+  state = {socket:null , globalNumber:0}
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   componentDidMount(){
     const socket = io('http://localhost:8888');
 
-    this.setState({socket})
+    this.setState({socket:socket})
 
-    socket.on('number:change', (globalNumber) => {
+    socket.on('number:change' , (globalNumber) => {
       this.setState({globalNumber})
     })
+    socket.on('user:new',(username) =>{
+      console.log('a user called ' + username + ' is connected')
+    })
 
+    socket.on ('user:me' , (username)=>{
+      this.setState({username})
+    })
+  }
+  handleChange = (evt) =>{
+    evt.preventDefault()
+    const input_value = evt.target.input.value
+    this.setState({input_value})
+    console.log(this.state.input_value);}
+
+  handleSubmit(evt) {
+    evt.preventDefault();
   }
 
   onIncrement = () => this.state.socket.emit('increment')
-
   onDecrement = () => this.state.socket.emit('decrement')
-
-  render(){
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title" >Welcome to Chat</h1>
         </header>
-	<h1>{this.state.globalNumber}</h1>
-        <p className="App-intro">
-          <button onClick={this.onIncrement}>+</button>
-          <button onClick={this.onDecrement}>-</button>
-        </p>
-      </div>
-    );
-  }
-}
-
+        {/* <h1>{this.state.globalNumber}</h1>
+        {/* <button onClick={this.onIncrement}>increament</button>
+        <br/>
+        <br/>
+        <button onClick={this.onDecrement}>decrement</button>
+        <br/>   */} 
+        <h1>{this.state.username}</h1>
+        <form onSubmit={this.handleChange}>
+        <input type='text' placeholder='Type Your Message' name='input'  />
+        <input type='submit' name='Submit'/>
+        </form>
+      </div> 
+     );
+   };
+  }; 
 export default App;
